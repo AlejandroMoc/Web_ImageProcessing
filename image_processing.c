d840312b18ca71f1ead9bbcc44c2a022256fdb38
Alejandro Moctezuma <moctezumaalejandro25@gmail.com>
2025-05-02 12:44:23 -0600

    Added process_functions.h, added basic file opening.

--- image_processing.c
+++ image_processing.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "process_functions.h"
 
 //gcc -fopenmp image_processing.c
 //./a.out
 
-//Funciones
-void gray_scale() {
-    printf("\nEn escala de grises\n");
-}
-
-void mirror_horizontal_color() {
-    printf("\nEn espejo respecto a la horizontal a color\n");
-}
-
-void mirror_vertical_color() {
-    printf("\nEn espejo respecto a la vertical a color\n");
-}
-
-void mirror_horizontal_gray() {
-    printf("\nEn espejo respecto a la horizontal en escala de grises\n");
-}
-
-void mirror_vertical_gray() {
-    printf("\nEn espejo respecto a la vertical en escala de grises\n");
-}
-
-void blurred_kernel() {
-    printf("\nEfecto de desenfoque con un kernel de 55 hasta 155\n");
-}
-
-
 //Ejecución principal
 int main() {
+
+    //Abrir archivo de reporte
+    FILE *report_file;
+    char data[80] = "report.txt";
+    report_file = fopen(data, "w");
+    if (report_file == NULL){
+        printf("Archivo nulo\n");
+        exit(1);
+    }
+
     //TODO abrir 100 imágenes
 
+    //Paralelización
     #pragma omp parallel
     {
         #pragma omp sections
@@ -41,37 +27,37 @@ int main() {
             //En escala de grises
             #pragma omp section
             {
-                (void)gray_scale();
+                (void)gray_scale("filepath");
             }
 
             //En espejo respecto de la horizontal a color
             #pragma omp section
             {
-                (void)mirror_horizontal_color();
+                (void)mirror_horizontal_color("filepath");
             }
 
             //En espejo respecto a la vertical a color
             #pragma omp section
             {
-                (void)mirror_vertical_color();
+                (void)mirror_vertical_color("filepath");
             }
 
             //En espejo respecto de la horizontal en escala de grises
             #pragma omp section
             {
-                (void)mirror_horizontal_gray();
+                (void)mirror_horizontal_gray("filepath");
             }
 
             //En espejo respecto a la vertical en escala de grises
             #pragma omp section
             {
-                (void)mirror_vertical_gray();
+                (void)mirror_vertical_gray("filepath");
             }
 
             //Efecto de desenfoque con un kernel de 55 hasta 155
             #pragma omp section
             {
-                (void)blurred_kernel();
+                (void)blurred_kernel("filepath");
             }
 
         }
@@ -79,8 +65,13 @@ int main() {
     
     int tiempo_ejecucion = 1;
 
-    //generar un archivo de texto de salida (.txt), donde indique el n´umero de localidades totales
-    //leidas por cada archivo original y las localidades totales de salida escritas despu´es del procesamiento
+    //TODO generar un archivo de texto de salida (.txt), donde indique el no. de localidades totales
+    //leidas por cada archivo original y las localidades totales de salida escritas después del procesamiento
+
+    fprintf(report_file, "Reporte de resultados\n");
+    fprintf(report_file, "Localidades leídas\n");
+    fprintf(report_file, "Localidades totales\n");
+    fclose(report_file);
 
     printf("El valor del tiempo es %d.\n", tiempo_ejecucion);
     return 0;
---
+++ process_functions.h
@@ -0,0 +1,24 @@
+//Funciones
+extern void gray_scale(char file_path[80]) {
+    printf("\nEn escala de grises\n");
+}
+
+extern void mirror_horizontal_color(char file_path[80]) {
+    printf("\nEn espejo respecto a la horizontal a color\n");
+}
+
+extern void mirror_vertical_color(char file_path[80]) {
+    printf("\nEn espejo respecto a la vertical a color\n");
+}
+
+extern void mirror_horizontal_gray(char file_path[80]) {
+    printf("\nEn espejo respecto a la horizontal en escala de grises\n");
+}
+
+extern void mirror_vertical_gray(char file_path[80]) {
+    printf("\nEn espejo respecto a la vertical en escala de grises\n");
+}
+
+extern void blurred_kernel(char file_path[80]) {
+    printf("\nEfecto de desenfoque con un kernel de 55 hasta 155\n");
+}
